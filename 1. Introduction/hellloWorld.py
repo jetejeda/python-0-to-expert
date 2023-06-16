af4ac6dff3c250ade5511b948ecfbfaed714bf83
jetejeda <eduardotejedaleon@gmail.com>
2023-06-16 17:20:56 -0600

    Python Fundamentals

---
+++ 1. Introduction/hellloWorld.py
@@ -0,0 +1,14 @@
+print("Hey!")
+
+'''Comment 
+using multiple
+lines'''
+
+print(((3+2)/(2*5))**2)
+
+clownWeight = 112#grams
+dollWeight = 75#grams
+clownQty = 23
+dollQty = 54
+print(clownWeight*clownQty + dollWeight*dollQty)
+
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # python-0-to-expert
 Python Udemy practice course
+
+# Vscode extensions needed
+
++ Python
