648008917ee025d74cd892cc3e0a188e49b12cb7
jetejeda <eduardotejedaleon@gmail.com>
2023-06-19 13:01:01 -0600

    Using format to display data

---
+++ 2. Reading data/Exercise 3.py
@@ -0,0 +1,10 @@
+lower = input('digit the text you want to convert to uppercase: ')
+upper = input('digit the text you want to convert to lowercase: ')
+print('Mixing the texts changing from lower to upper case: ', upper.lower(), lower.upper(), '\n')
+
+print('Exercise 4: \n')
+name = input('Type your name: ')
+age = input('Type your age: ')
+gender = input('Type your gender: ')
+
+print('Your name is: {} \n your age is: {} \n your gender is: {}'.format(name, age, gender))
