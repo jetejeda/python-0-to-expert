4465b0914acffff5e96baf52038bfd5627fb4b25
jetejeda <eduardotejedaleon@gmail.com>
2023-06-22 16:17:32 -0600

    Introduction to classes

---
+++ 7. Object Oriented Programming/Classes.py
@@ -0,0 +1,9 @@
+class Phone():
+    pass#Default value so that it don't throw an error.
+
+class Person():
+    pass
+    def sayHello(self, message):
+        return message
+firstPhone = Phone()
+print(type(firstPhone))
