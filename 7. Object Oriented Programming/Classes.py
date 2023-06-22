29f272d04e77d7a4f595a1af8662c4b79fbfd32e
jetejeda <eduardotejedaleon@gmail.com>
2023-06-22 16:44:09 -0600

    Init and self explanation

--- 7. Object Oriented Programming/Classes.py
+++ 7. Object Oriented Programming/Classes.py
@@ -2,8 +2,15 @@ class Phone():
     pass#Default value so that it don't throw an error.
 
 class Person():
-    pass
+    def __init__(self, name, age) -> None:
+        self.name = name
+        self.age = age
+
     def sayHello(self, message):
-        return message
+        return 'Hey, my name is {} and I am {} years old, you said: {}'.format(self.name, self.age, message)
+
 firstPhone = Phone()
 print(type(firstPhone))
+
+firstPerson = Person('Jose', '23')
+print(firstPerson.sayHello('Hi!'))
