b6d87f9b18405a4e508ce11544dba039a84fa680
jetejeda <eduardotejedaleon@gmail.com>
2023-06-23 12:08:51 -0600

    Encapsulation and its best practices

--- 7. Object Oriented Programming/Classes.py
+++ 7. Object Oriented Programming/Classes.py
@@ -5,6 +5,8 @@ class Person():
     def __init__(self, name, age) -> None:
         self.name = name
         self.age = age
+        self.__Id = age + 1#Defining a private attribute
+        self._description = 'Another way to indicate private attributes'# Not to python but to another programmer
 
     def __str__(self):
         return 'I am {} and this is my description'.format(self.name)
@@ -14,9 +16,21 @@ class Person():
     
     def __del__(self):
         print('I am {} and I just wanted to say good bye!'.format(self.name))
+    
+    @property
+    def description(self):
+        return self._description
+
+    @description.setter
+    def description(self, description):
+        self._description = description
+
 firstPhone = Phone()
 print(type(firstPhone))
 
-firstPerson = Person('Jose', '23')
+firstPerson = Person('Jose', 23)
 print(firstPerson.sayHello('Hi!'))
 print(firstPerson)
+print('We used a getter method to access this attribute: ', firstPerson.description)
+firstPerson.description = 'New description using setter method'
+print('We used a setter method to modify this attribute: ', firstPerson.description)
